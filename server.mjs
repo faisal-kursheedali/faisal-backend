a11d9e85c7f7caab8562ad9efedecb04c7f621fb
FAISAL <85350108+faisal-kursheedali@users.noreply.github.com>
2024-03-07 16:54:53 +0530

    ip address fetching done

--- server.mjs
+++ server.mjs
@@ -1,9 +1,7 @@
 import express from "express";
 import addData from "./util/addData.js";
-// import getIp from "./util/ip";
 import addUser from "./util/addUser.js";
 import getIp from "./util/ip.js";
-import bodyParser from "body-parser";
 import cors from "cors";
 import { getOptions } from "./util/options.js";
 
@@ -11,15 +9,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-// For parsing application/json
 app.use(express.json());
-
-// app.use(bodyParser.json());
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-
-// // For parsing application/x-www-form-urlencoded
-// app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/actions", async (req, response) => {
   try {
@@ -43,17 +33,13 @@ app.post("/api/users", async (req, response) => {
     const newUser = await addUser(ip, reqBody.date);
     if (newUser === "user_onboard") {
       return response.status(200).json({ message: "New user created" });
-      // return res.json(200, { message: "New user created" });
     } else if (newUser === "user_visit") {
       return response.status(200).json({ message: "Existing user" });
-      // return res.json(200, { message: "Existing user" });
     }
     return response.status(400).json({ message: "Some thing went wrong" });
-    // return res.json({ message: "Some thing went wrong" }, { status: 400 });
   } catch (e) {
     console.log(e);
     return response.status(400).json({ message: "Some thing went wrong" });
-    // return res.json({ message: "Some thing went wrong" }, { status: 400 });
   }
 });
 
--- util/ip.js
+++ util/ip.js
@@ -1,10 +1,11 @@
+const requestIP = require("request-ip");
+
 const getIp = (req) => {
   if (process.env.ENVIRONMENT === "DEVELOPMENT") {
     return "49.37.192.155";
   } else {
-    const ip = (req.headers.get("x-forwarded-for") ?? "127.0.0.1").split(
-      ","
-    )[0];
+    const ip = requestIP.getClientIp(req);
+
     return ip;
   }
 };
